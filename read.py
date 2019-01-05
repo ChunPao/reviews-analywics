24b4e29227ba78409a1c042f9eda6a0c10539c91
ChunPao <berger3280@gmail.com>
2019-01-05 21:52:08 +0800

    twise commit

--- read.py
+++ read.py
@@ -5,4 +5,11 @@ with open('reviews.txt', 'r') as f:
         data.append(line)
         #print(len(data))
         if count % 1000 == 0: # %求餘數
-print(len(data))
+            print(len(data))
+print('檔案讀取完了，總共有', len(data), '比資料')
+
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+    print(sum_len)
+print('留言的平均長度為'， sum_len/len(data))
